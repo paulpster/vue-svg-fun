3d7498dadc20a376eba358eb29adf57ba13ba422
Paul Power <paulpster@gmail.com>
2021-02-19 10:21:16 -0700

    fixed list styling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -48,12 +48,7 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  text-align: initial;
 }
 a {
   color: #42b983;
