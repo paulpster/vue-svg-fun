898c898382a2e6312bf40286ae0dba0ca00bd417
Paul Power <paulpster@gmail.com>
2021-02-26 09:23:00 -0700

    added some math explanation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="about">
     <HelloWorld msg="My About Page" />
+    <CirclesHelp />
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue";
+import CirclesHelp from "@/components/CirclesHelp.vue";
 
-@Component({ components: { HelloWorld } })
+@Component({ components: { HelloWorld, CirclesHelp } })
 export default class About extends Vue {}
 </script>
