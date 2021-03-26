8ae4d1034667b400c41122f64de5c20cff22abe4
Paul Power <paulpster@gmail.com>
2021-03-26 12:13:18 -0600

    Component ro help me with the layout i want

---
+++ src/components/ContentLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cont">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="incont">
+      <div>
+        <slot></slot>
+      </div>
+      <div class="side noprint">
+        <slot name="side"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.incont {
+  display: flex;
+}
+.side {
+  padding: 0.5rem;
+}
+</style>
