d4de881617eff13d301db892d01f35dfb38c2975
Paul Power <paulpster@gmail.com>
2021-03-26 12:16:07 -0600

    added some info for future implemtation

--- src/views/Penrose.vue
+++ src/views/Penrose.vue
@@ -3,3 +3,11 @@
     <h2>Penrose Tiling</h2>
   </div>
 </template>
+
+<script lang="ts">
+//https://en.wikipedia.org/wiki/Penrose_tiling
+//http://www.math.ubc.ca/~cass/courses/m308-02b/projects/schweber/penrose.html
+
+import { Vue } from "vue-property-decorator";
+export default class Penrose extends Vue {}
+</script>
